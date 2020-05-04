af02f33ad27a2391c2d6e24cb147c4b660dfd7ab
tantarin <tanya.tarinskaya@gmail.com>
2020-05-04 13:05:59 +0300

    delete common code method

--- src/test/java/medapp/PatientServiceTest.java
+++ src/test/java/medapp/PatientServiceTest.java
@@ -10,13 +10,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 
